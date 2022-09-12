2b8523f758bb98d230829e88384af98647a4253d
Borja <borjadg38@gmail.com>
2022-09-12 16:15:50 +0100

    Update classes

--- app/src/androidTest/java/helloAr/testAgentSupport/MyAgentState.java
+++ app/src/androidTest/java/helloAr/testAgentSupport/MyAgentState.java
@@ -1,5 +1,8 @@
 package helloAr.testAgentSupport;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import eu.iv4xr.framework.extensions.pathfinding.Navigatable;
 import eu.iv4xr.framework.mainConcepts.Iv4xrAgentState;
 import nl.uu.cs.aplib.mainConcepts.Environment;
@@ -37,10 +40,36 @@ public class MyAgentState extends Iv4xrAgentState<Void> {
     public void updateState(String agentId) {
         super.updateState(agentId);
         // remove anchors that are no longer there:
-        // wrong logic! we should remove anchors whose id is NOT in lastseen.
-        for(String id : env().lastSeen) {
+        // wrong logic!
+        /*for(String id : env().lastSeen) {
             this.worldmodel.elements.remove(id) ;
+        }*/
+
+        //New: Removed anchors whose id is NOT in lastseen
+
+        //New: Split in two parts:
+        // 1: Store the elements to remove;
+        // 2: Remove them
+        boolean isInLastSeen = false;
+        List<String> elementsToRemove = new ArrayList<String>();
+
+        for(String idx : this.worldmodel.elements.keySet()) {
+            for (String id : env().lastSeen) {
+                if(idx.equals(id)) {
+                    isInLastSeen = true;
+                    break;
+                }
+            }
+            if(isInLastSeen == false) {
+                elementsToRemove.add(idx);
+            }
+            isInLastSeen = false;
         }
+
+        for(String element : elementsToRemove) {
+            this.worldmodel.elements.remove(element);
+        }
+
     }
 
 }
